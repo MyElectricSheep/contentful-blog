67e21d98859ab6eac3449e24c7ab9fc9a92c5566
Ben Paget <ben.paget@live.fr>
2020-05-10 14:22:55 +0100

    modified handling of env variables

--- src/config/contentful.js
+++ src/config/contentful.js
@@ -1,7 +1,7 @@
 // const contentful = require("contentful");
 import * as contentful from 'contentful';
 
-let accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
+// let accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
 
 // if (process.env.NODE_ENV !== 'production') {
 //   accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
@@ -10,5 +10,5 @@ let accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
 export default contentful.createClient({
   space: "n6dovmdnj5ms",
   environment: "master", // defaults to 'master' if not set
-  accessToken,
+  accessToken: "AXOAATFv9av1tVsHIbDIJ1_ZF8iMg9Wkj8NsJy41uks",
 });
